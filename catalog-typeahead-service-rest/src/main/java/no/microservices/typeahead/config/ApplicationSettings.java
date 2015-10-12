28a6289bac9bdc1176069f8bfc222246a5cbd752
Andreas Bjørnådal <bjornadal@gmail.com>
2015-10-12 12:55:26 +0200

    Initial commit

---
+++ catalog-typeahead-service-rest/src/main/java/no/microservices/typeahead/config/ApplicationSettings.java
@@ -0,0 +1,10 @@
+package no.microservices.typeahead.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "microservice")
+public class ApplicationSettings {
+
+}
